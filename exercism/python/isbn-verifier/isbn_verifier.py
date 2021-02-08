e8f0524a5331722ccb86f9fef095eb1346ece1f9
SeanPhilippi <sean.philippi@gmail.com>
2021-02-07 22:17:01 -0600

    need to account for x = 10

--- exercism/python/isbn-verifier/isbn_verifier.py
+++ exercism/python/isbn-verifier/isbn_verifier.py
@@ -2,9 +2,25 @@
 # if an x at the end, it represents 10
 # ignore - characters
 def is_valid(isbn):
-    isbn.replace('-', '')
-    nums = isbn.split()
-    ints = list(map(int, nums))
+    nums = isbn.replace('-', '')
+    print('nums', nums)
+    # do regex check for digits 0-9 or x or X only
+    # if nums[9] 
+    if len(nums) != 10:
+        return False
+    ints = []
+    for num in nums:
+        ints.append(int(num))
+    # ints = list(map(int, nums))
+    print('ints', ints)
     # do calculation using array of nums
+    product = 0
+    multiplier = 10
+    for num in ints:
+        product += num * multiplier 
+        multiplier -= 1
+    product = product % 11
+    if product == 0:
+        return True
 
-    
+print(is_valid('3-598-21508-9'))
