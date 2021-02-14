513773b82986999d6554e09bf415672dda190e04
SeanPhilippi <sean.philippi@gmail.com>
2021-02-14 08:17:03 -0600

    changed for loops to list comprehensions

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -4,17 +4,10 @@ class Matrix:
     def __init__(self, matrix_string):
         rows_arr = matrix_string.splitlines()
         sep_nums = list(map(methodcaller('split', ' '), rows_arr))
-        matrix = []
-        for arr in sep_nums:
-            digits = list(map(int, arr))
-            matrix.append(digits)
-        self.matrix = matrix
+        self.matrix = [list(map(int, arr)) for arr in sep_nums]
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        col = []
-        for row in self.matrix:
-            col.append(row[index - 1])
-        return col
+        return [row[index - 1] for row in self.matrix]
