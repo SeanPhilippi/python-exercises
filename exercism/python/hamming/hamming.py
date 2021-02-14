27ef6af2c44e48e4eb612a0f0b8938ee300e35ce
SeanPhilippi <sean.philippi@gmail.com>
2021-02-14 15:47:13 -0600

    simplified

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,12 +1,9 @@
 def distance(strand_a, strand_b):
-    try:
-        if len(strand_a) == len(strand_b):
-            distance = 0
-            for i, ch in enumerate(strand_a):
-                if ch != strand_b[i]:
-                    distance += 1
-            return distance
-        else:
-            raise ValueError('strand_a and strand_b are not of equal length')
-    except ValueError as e:
-        raise ValueError(e)
+    if len(strand_a) == len(strand_b):
+        distance = 0
+        for i, ch in enumerate(strand_a):
+            if ch != strand_b[i]:
+                distance += 1
+        return distance
+    else:
+        raise ValueError('strand_a and strand_b are not of equal length')
