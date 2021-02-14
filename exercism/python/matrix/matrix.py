8fff1ff469416e238061b1ca34bbc9dd074fef94
SeanPhilippi <sean.philippi@gmail.com>
2021-02-14 14:20:51 -0600

    more minimizing

--- exercism/python/matrix/matrix.py
+++ exercism/python/matrix/matrix.py
@@ -1,7 +1,8 @@
 class Matrix:
     def __init__(self, matrix_string):
-        sep_nums = [row.split(' ') for row in matrix_string.splitlines()]
-        self.matrix = [list(map(int, arr)) for arr in sep_nums]
+        sep_nums = [row.split() for row in matrix_string.splitlines()]
+        # self.matrix = [list(map(int, arr)) for arr in sep_nums]
+        self.matrix = [[int(i) for i in arr] for arr in sep_nums]
 
     def row(self, index):
         return self.matrix[index - 1]
