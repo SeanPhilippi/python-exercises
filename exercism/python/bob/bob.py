eaec248b01bc728cf9a67fe617cea11533f6afb5
SeanPhilippi <sean.philippi@gmail.com>
2021-03-02 20:44:52 -0600

    cleaned up

--- exercism/python/bob/bob.py
+++ exercism/python/bob/bob.py
@@ -1,17 +1,11 @@
 def response(hey_bob):
-    # simple question, 'Sure.' not uppercased
     if hey_bob.strip().endswith('?') and not hey_bob.isupper():
         return 'Sure.'
-    # no characters, only white space in string
     elif hey_bob.isspace() or hey_bob == '':
         return 'Fine. Be that way!'
-    # all caps, 'Whoa, chill out!'
-    elif hey_bob.isupper() and not hey_bob.endswith('?'):
+    elif hey_bob.isupper() and not hey_bob.strip().endswith('?'):
         return 'Whoa, chill out!'
-    # all caps question, 'Calm down, I know what I'm doing!' 
     elif hey_bob.isupper() and hey_bob.strip().endswith('?'):
         return "Calm down, I know what I'm doing!"
-    # default 'Whatever.'
     else:
         return 'Whatever.'
-
