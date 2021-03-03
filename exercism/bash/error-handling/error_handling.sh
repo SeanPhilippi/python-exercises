0bac3156e03d56f3c853c994c4f3eb7ccbb4b53b
SeanPhilippi <sean.philippi@gmail.com>
2021-03-02 18:31:49 -0600

    improvements for error_handling.sh

--- exercism/bash/error-handling/error_handling.sh
+++ exercism/bash/error-handling/error_handling.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ] || [ $# -eq 2 ]; then
+if [[ $# -ne 1 ]]; then
     echo "Usage: error_handling.sh <person>"
     exit 1
-else
-    echo "Hello, $1"
 fi
+echo "Hello, $1"
