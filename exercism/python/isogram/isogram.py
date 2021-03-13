3e1cb03862973ea599b5dbb28b567445b89a89b9
SeanPhilippi <sean.philippi@gmail.com>
2021-03-13 13:13:37 -0600

    isogram simplified

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -1,13 +1,10 @@
 def is_isogram(string):
-    counts = {}
-    new_string = string.lower().replace('-', '').replace(' ', '')
+    counts = set()
 
-    for ch in set(new_string):
-        counts[ch] = 0
-    
-    for ch in new_string:
-        if counts[ch] == 1:
-            return False
-        else:
-            counts[ch] += 1  
+    for ch in string.lower():
+        if ch.isalpha():
+            if ch not in counts:
+                counts.add(ch)  
+            else:
+                return False
     return True
