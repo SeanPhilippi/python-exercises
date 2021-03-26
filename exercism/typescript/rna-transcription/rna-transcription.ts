b4cdc86eddd56825e173b7214cf9d1bf0f75db96
SeanPhilippi <sean.philippi@gmail.com>
2021-03-26 18:43:46 -0500

    passing all tests for rna-transcription

---
+++ exercism/typescript/rna-transcription/rna-transcription.ts
@@ -0,0 +1,28 @@
+class Transcriptor {
+  private rnaHash: {
+    [key: string]: string;
+  }
+
+  constructor() {
+    this.rnaHash = {
+      G: 'C',
+      C: 'G',
+      T: 'A',
+      A: 'U',
+    }
+  }
+
+  toRna = (dna: string): string => {
+    const rnaArray = [];
+    for (const letter of dna) {
+      if (letter in this.rnaHash) {
+        rnaArray.push(this.rnaHash[letter]);
+      } else {
+        throw new Error('Invalid input DNA.');
+      }
+    }
+    return rnaArray.join('');
+  }
+}
+
+export default Transcriptor
